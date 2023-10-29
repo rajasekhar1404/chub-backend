9e542f6c6fc0f7cf8d8bf879342c0aeef5650e54
Rajasekhar <rajasekhar@gmail.com>
2023-10-30 00:50:15 +0530

    Socket message type added

--- webSocket/WebSocketEventHandler.js
+++ webSocket/WebSocketEventHandler.js
@@ -5,14 +5,14 @@ const onConnection = (ws, wss) => {
     ws.on('message', function message(data) {
         wss.clients.forEach(client => {
             const messageObject = JSON.parse(data)
-            if (client !== ws && client.protocol === messageObject.userId) {
+            if (client !== ws && client.protocol === messageObject.data.userId) {
                 client.send(data.toString())
             }
         })
     })
 
-    ws.on('close', function closed(c, d) {
-        console.log(c, d.toString())
+    ws.on('close', function closed(code, reason) {
+       
     })
 
 }
